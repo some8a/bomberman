f6689bc9bb4e2676c3e1d1e46eb86fecff39669d
some8a <67367308+some8a@users.noreply.github.com>
2021-07-09 14:14:06 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,198 +1,62 @@
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    for (let index = 0; index < 2; index++) {
-        mySprite.setImage(img`
-            . . . . . . . . . . . . . 
-            . . . f f f f f f . . . . 
-            . f f f f f f f f f . . . 
-            . f f f f f f c f f f . . 
-            f f f f c f f f c f f f . 
-            f c f f c c f f f c c f f 
-            f c c f f f f e f f f f f 
-            f f f f f f f e e f f f . 
-            f f e e f b f e e f f f . 
-            f f e 4 e 1 f 4 4 f f . . 
-            . f f f e 4 4 4 4 f . . . 
-            . 4 4 4 e e e e f f . . . 
-            . e 4 4 e 7 7 7 7 f . . . 
-            . f e e f 6 6 6 6 f f . . 
-            . f f f f f f f f f f . . 
-            . . f f . . . f f f . . . 
-            `)
-        mySprite.x += 4
-        pause(100)
-        scene.centerCameraAt(mySprite.x, mySprite.y)
-        mySprite.setImage(img`
-            . . . . . . . . . . . . . 
-            . . . f f f f f f . . . . 
-            . f f f f f f f f f . . . 
-            . f f f f f f c f f f . . 
-            f f f f c f f f c f f f . 
-            f c f f c c f f f c c f f 
-            f c c f f f f e f f f f f 
-            f f f f f f f e e f f f . 
-            f f e e f b f e e f f . . 
-            . f e 4 e 1 f 4 4 f f . . 
-            . f f f e e 4 4 4 f . . . 
-            . . f e 4 4 e e f f . . . 
-            . . f e 4 4 e 7 7 f . . . 
-            . f f f e e f 6 6 f f . . 
-            . f f f f f f f f f f . . 
-            . . f f . . . f f f . . . 
-            `)
-        mySprite.x += 4
-        pause(100)
-        scene.centerCameraAt(mySprite.x, mySprite.y)
-    }
-    if (mySprite.overlapsWith(renga)) {
-        mySprite.x += -16
-    }
-})
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    for (let index = 0; index < 2; index++) {
-        if (mySprite.overlapsWith(renga)) {
-            break;
-        }
-        mySprite.setImage(img`
-            . . . . . . . . . . . . . 
-            . . . . . f f f f . . . . 
-            . . . f f f f f f f f . . 
-            . . f f f f f f c f f f . 
-            f f f f f f f c c f f f c 
-            f f f f c f f f f f f f c 
-            . c c c f f f e e f f c c 
-            . f f f f f e e f f c c f 
-            . f f f b f e e f b f f f 
-            . f f 4 1 f 4 4 f 1 4 f f 
-            . . f e 4 4 4 4 4 e e f e 
-            . f e f b 7 7 7 e 4 4 4 e 
-            . e 4 f 7 7 7 7 e 4 4 e . 
-            . . . f 6 6 6 6 6 e e . . 
-            . . . f f f f f f f . . . 
-            . . . f f f . . . . . . . 
-            `)
-        mySprite.y += 4
-        pause(100)
-        scene.centerCameraAt(mySprite.x, mySprite.y)
-        mySprite.setImage(img`
-            . . . . . . . . . . . . . 
-            . . . . f f f f . . . . . 
-            . . f f f f f f f f . . . 
-            . f f f c f f f f f f . . 
-            c f f f c c f f f f f f f 
-            c f f f f f f f c f f f f 
-            c c f f e e f f f c c c . 
-            f c c f f e e f f f f f . 
-            f f f b f e e f b f f f . 
-            f f 4 1 f 4 4 f 1 4 f f . 
-            e f e e 4 4 4 4 4 e f . . 
-            e 4 4 4 e 7 7 7 b f e f . 
-            . e 4 4 e 7 7 7 7 f 4 e . 
-            . . e e 6 6 6 6 6 f . . . 
-            . . . f f f f f f f . . . 
-            . . . . . . . f f f . . . 
-            `)
-        mySprite.y += 4
-        pause(100)
-        scene.centerCameraAt(mySprite.x, mySprite.y)
-    }
+    mySprite.setImage(img`
+        . . . . . . . . . . . . . 
+        . . . . . f f f f . . . . 
+        . . . f f f f f f f f . . 
+        . . f f f f f f c f f f . 
+        . f f f f f f c c f f f . 
+        . f f f c f f f f f f f . 
+        . c c c f f f e e f f c . 
+        . f f f f f e e f f c c . 
+        . f f f b f e e f b f f . 
+        . f f 4 1 f 4 4 f 1 4 f . 
+        . . f e 4 4 4 4 4 e e f . 
+        . f e f b 7 7 7 e 4 4 4 . 
+        . e 4 f 7 7 7 7 e 4 4 e . 
+        . . . f 6 6 6 6 6 e e . . 
+        . . . f f f f f f f . . . 
+        . . . . . . . . . . . . . 
+        `)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    for (let index = 0; index < 2; index++) {
-        if (mySprite.overlapsWith(renga)) {
-            break;
-        }
-        mySprite.setImage(img`
-            . . . . . . . . . . . . . 
-            . . . . f f f f f f . . . 
-            . . . f f f f f f f f f . 
-            . . f f f c f f f f f f . 
-            . f f f c f f f c f f f f 
-            f f c c f f f c c f f c f 
-            f f f f f e f f f f c c f 
-            . f f f e e f f f f f f f 
-            . . f f e e f b f e e f f 
-            . . f f 4 4 f 1 e 4 e f . 
-            . . . f 4 4 4 e e f f f . 
-            . . . f f e e 4 4 e f . . 
-            . . . f 7 7 e 4 4 e f . . 
-            . . f f 6 6 f e e f f f . 
-            . . f f f f f f f f f f . 
-            . . . f f f . . . f f . . 
-            `)
-        mySprite.x += -4
-        pause(100)
-        scene.centerCameraAt(mySprite.x, mySprite.y)
-        mySprite.setImage(img`
-            . . . . . . . . . . . . . 
-            . . . . f f f f f f . . . 
-            . . . f f f f f f f f f . 
-            . . f f f c f f f f f f . 
-            . f f f c f f f c f f f f 
-            f f c c f f f c c f f c f 
-            f f f f f e f f f f c c f 
-            . f f f e e f f f f f f f 
-            . f f f e e f b f e e f f 
-            . . f f 4 4 f 1 e 4 e f f 
-            . . . f 4 4 4 4 e f f f . 
-            . . . f f e e e e 4 4 4 . 
-            . . . f 7 7 7 7 e 4 4 e . 
-            . . f f 6 6 6 6 f e e f . 
-            . . f f f f f f f f f f . 
-            . . . f f f . . . f f . . 
-            `)
-        mySprite.x += -4
-        pause(100)
-        scene.centerCameraAt(mySprite.x, mySprite.y)
-    }
+    mySprite.setImage(img`
+        . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . 
+        . . . f f f f f f f f f . 
+        . . f f f c f f f f f f . 
+        . f f f c f f f c f f f . 
+        . f c c f f f c c f f c . 
+        . f f f f e f f f f c c . 
+        . f f f e e f f f f f f . 
+        . . f f e e f b f e e f . 
+        . . f f 4 4 f 1 e 4 e f . 
+        . . . f 4 4 4 e e f f f . 
+        . . . f f e e 4 4 e f . . 
+        . . . f 7 7 e 4 4 e f . . 
+        . . f f 6 6 f e e f f f . 
+        . . f f f f f f f f f f . 
+        . . . . . . . . . . . . . 
+        `)
 })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    for (let index = 0; index < 2; index++) {
-        if (mySprite.overlapsWith(renga)) {
-            break;
-        }
-        mySprite.setImage(img`
-            . . . . . . . . . . . . . 
-            . . . . . f f f f . . . . 
-            . . . f f c c c c f f . . 
-            . f f f c c c c c c f f . 
-            f f c c c c c c c c c f f 
-            f c c c c f c c c c c c f 
-            . f f f f c c c c f c c f 
-            . f f f f c c f c c c f f 
-            . f f f f f f f f f f f f 
-            . f f f f f f f f f f f f 
-            . . f f f f f f f f f f . 
-            . . e f f f f f f f f f . 
-            . . e f f f f f f f f e f 
-            . . 4 c 7 7 7 7 7 e 4 4 e 
-            . . e f f f f f f f e e . 
-            . . . f f f . . . . . . . 
-            `)
-        mySprite.y += -4
-        pause(100)
-        scene.centerCameraAt(mySprite.x, mySprite.y)
-        mySprite.setImage(img`
-            . . . . . . . . . . . . . 
-            . . . . . f f f f . . . . 
-            . . . f f c c c c f f . . 
-            . . f f c c c c c c f f . 
-            . f f f c c c c c c c f f 
-            f f f c c c c c c c c c f 
-            f f c c c f c c c c c c f 
-            . f f f f f c c c f c f f 
-            . f f f f c c f f c f f f 
-            . . f f f f f f f f f f f 
-            . . f f f f f f f f f f . 
-            . . f f f f f f f f f e . 
-            . f e f f f f f f f f e . 
-            . e 4 4 e 7 7 7 7 7 c 4 . 
-            . . e e f f f f f f f e . 
-            . . . . . . . . f f f . . 
-            `)
-        mySprite.y += -4
-        pause(100)
-        scene.centerCameraAt(mySprite.x, mySprite.y)
-    }
+    mySprite.setImage(img`
+        . . . . . . . . . . . . . 
+        . . . . . f f f f . . . . 
+        . . . f f c c c c f f . . 
+        . f f f c c c c c c f f . 
+        . f c c c c c c c c c f . 
+        . c c c c f c c c c c c . 
+        . f f f f c c c c f c c . 
+        . f f f f c c f c c c f . 
+        . f f f f f f f f f f f . 
+        . f f f f f f f f f f f . 
+        . . f f f f f f f f f f . 
+        . . e f f f f f f f f f . 
+        . . e f f f f f f f f e . 
+        . . 4 c 7 7 7 7 7 e 4 4 . 
+        . . e f f f f f f f e e . 
+        . . . . . . . . . . . . . 
+        `)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite2 = sprites.create(img`
@@ -219,37 +83,11 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 let mySprite2: Sprite = null
 let mySprite: Sprite = null
-let renga: Sprite = null
+let カウンター = 0
+let renga2 = null
+let counterX = 0
 scene.setBackgroundColor(6)
 tiles.setTilemap(tilemap`レベル1`)
-for (let counterX = 0; counterX <= 12; counterX++) {
-    for (let counterY = 0; counterY <= 13; counterY++) {
-        if (!(counterX % 2 == 0 && counterY % 2 == 1)) {
-            if (Math.percentChance(60)) {
-                renga = sprites.create(img`
-                    e e e e f e e e e e e e e e e e 
-                    e e e e f e e e e e e e e e e e 
-                    e e e e f e e e e e e e e e e e 
-                    f f f f f f f f f f f f f f f f 
-                    e e e e e e e e e e e f e e e e 
-                    e e e e e e e e e e e f e e e e 
-                    e e e e e e e e e e e f e e e e 
-                    e e e e e e e e e e e f e e e e 
-                    f f f f f f f f f f f f f f f f 
-                    e e e e f e e e e e e e e e e e 
-                    e e e e f e e e e e e e e e e e 
-                    e e e e f e e e e e e e e e e e 
-                    f f f f f f f f f f f f f f f f 
-                    e e e e e e e e e e e f e e e e 
-                    e e e e e e e e e e e f e e e e 
-                    e e e e e e e e e e e f e e e e 
-                    `, SpriteKind.Projectile)
-                renga.setPosition(counterX * 16 + 40, counterY * 16 + 24)
-            }
-        }
-    }
-}
-let カウンター = 0
 mySprite = sprites.create(img`
     . . . . f f f f . . . . . 
     . . f f f f f f f f . . . 
@@ -269,3 +107,5 @@ mySprite = sprites.create(img`
     . . . f f . . f f . . . . 
     `, SpriteKind.Player)
 mySprite.setPosition(24, 24)
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
