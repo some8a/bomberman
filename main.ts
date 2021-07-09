b38fca3babd0627b5c932077af8afc9d0995ac66
some8a <67367308+some8a@users.noreply.github.com>
2021-07-09 19:30:25 +0900

    ブロックめり込まない

--- main.ts
+++ main.ts
@@ -4,20 +4,20 @@ namespace SpriteKind {
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(img`
         . . . . . . . . . . . . . 
-        . . . . . f f f f . . . . 
-        . . . f f f f f f f f . . 
-        . . f f f f f f c f f f . 
-        . f f f f f f c c f f f . 
-        . f f f c f f f f f f f . 
-        . c c c f f f e e f f c . 
-        . f f f f f e e f f c c . 
-        . f f f b f e e f b f f . 
-        . f f 4 1 f 4 4 f 1 4 f . 
-        . . f e 4 4 4 4 4 e e f . 
-        . f e f b 7 7 7 e 4 4 4 . 
-        . e 4 f 7 7 7 7 e 4 4 e . 
-        . . . f 6 6 6 6 6 e e . . 
-        . . . f f f f f f f . . . 
+        . . . f f f f f f . . . . 
+        . f f f f f f f f f . . . 
+        . f f f f f f c f f f . . 
+        . f f f c f f f c f f f . 
+        . c f f c c f f f c c f . 
+        . c c f f f f e f f f f . 
+        . f f f f f f e e f f f . 
+        . f e e f b f e e f f . . 
+        . f e 4 e 1 f 4 4 f f . . 
+        . f f f e e 4 4 4 f . . . 
+        . . f e 4 4 e e f f . . . 
+        . . f e 4 4 e 7 7 f . . . 
+        . f f f e e f 6 6 f f . . 
+        . f f f f f f f f f f . . 
         . . . . . . . . . . . . . 
         `)
 })
@@ -87,12 +87,12 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.renga, function (sprite, otherSp
     if (cX < 8) {
         mySprite.x += 0 - cX
     } else {
-        mySprite.x += 0 - cX
+        mySprite.x += 16 - cX
     }
-    if (cY == 0) {
+    if (cY < 8) {
         mySprite.y += 0 - cY
     } else {
-        mySprite.y += 0 - cY
+        mySprite.y += 16 - cY
     }
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
