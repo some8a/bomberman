d4ca2b1924668c941727e7d870daa48bb018efe5
some8a <67367308+some8a@users.noreply.github.com>
2021-07-21 14:03:10 +0900

    更新 pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -32,6 +32,36 @@ sprites.onOverlap(SpriteKind.fire, SpriteKind.itemNoFiteDamage, function (sprite
         `)
     tiles.setWallAt(tiles.getTileLocation(otherSprite.x / 16, otherSprite.y / 16), false)
 })
+function clearStage () {
+    for (let 値 of sprites.allOfKind(SpriteKind.renga)) {
+        値.destroy()
+    }
+    for (let 値 of sprites.allOfKind(SpriteKind.spriteItem1)) {
+        値.destroy()
+    }
+    for (let 値 of sprites.allOfKind(SpriteKind.spriteItem)) {
+        値.destroy()
+    }
+    for (let 値 of sprites.allOfKind(SpriteKind.Projectile)) {
+        値.destroy()
+    }
+    for (let 値 of sprites.allOfKind(SpriteKind.fire)) {
+        値.destroy()
+    }
+    for (let 値 of sprites.allOfKind(SpriteKind.Enemy)) {
+        値.destroy()
+    }
+    for (let 値 of sprites.allOfKind(SpriteKind.goal)) {
+        値.destroy()
+    }
+    for (let 値 of sprites.allOfKind(SpriteKind.Player)) {
+        値.destroy()
+    }
+    for (let 値 of sprites.allOfKind(SpriteKind.Enemy)) {
+        値.destroy()
+    }
+    tiles.setTilemap(tilemap`レベル1`)
+}
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(img`
         . . . . . . . . . . . . . 
@@ -58,6 +88,107 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         mySprite.y += 32 - (mySprite.y - 24) % 32
     }
 })
+function setSprite () {
+    mySprite = sprites.create(img`
+        . . . . f f f f . . . . . 
+        . . f f f f f f f f . . . 
+        . f f f f f f c f f f . . 
+        f f f f f f c c f f f c . 
+        f f f c f f f f f f f c . 
+        c c c f f f e e f f c c . 
+        f f f f f e e f f c c f . 
+        f f f b f e e f b f f f . 
+        . f 4 1 f 4 4 f 1 4 f . . 
+        . f e 4 4 4 4 4 4 e f . . 
+        . f f f e e e e f f f . . 
+        f e f b 7 7 7 7 b f e f . 
+        e 4 f 7 7 7 7 7 7 f 4 e . 
+        e e f 6 6 6 6 6 6 f e e . 
+        . . . f f f f f f . . . . 
+        . . . f f . . f f . . . . 
+        `, SpriteKind.Player)
+    mySprite.setPosition(24, 24)
+    controller.moveSprite(mySprite)
+    scene.cameraFollowSprite(mySprite)
+    numOfEnemy = stageLevel
+    onlyStart = true
+    if (stageLevel > 4) {
+        numOfEnemy += 1
+        enemy1 = sprites.create(img`
+            ........................
+            ........................
+            ........................
+            ........................
+            ..........ffff..........
+            ........ff1111ff........
+            .......fb111111bf.......
+            .......f11111111f.......
+            ......fd11111111df......
+            ......fd11111111df......
+            ......fddd1111dddf......
+            ......fbdbfddfbdbf......
+            ......fcdcf11fcdcf......
+            .......fb111111bf.......
+            ......fffcdb1bdffff.....
+            ....fc111cbfbfc111cf....
+            ....f1b1b1ffff1b1b1f....
+            ....fbfbffffffbfbfbf....
+            .........ffffff.........
+            ...........fff..........
+            ........................
+            ........................
+            ........................
+            ........................
+            `, SpriteKind.Enemy2)
+        tiles.placeOnRandomTile(enemy1, assets.tile`transparency16`)
+        enemy1.follow(mySprite, 20)
+    }
+    if (stageLevel > 3) {
+        numOfEnemy += 1
+        enemy1 = sprites.create(img`
+            . . f f f . . . . . . . . f f f 
+            . f f c c . . . . . . f c b b c 
+            f f c c . . . . . . f c b b c . 
+            f c f c . . . . . . f b c c c . 
+            f f f c c . c c . f c b b c c . 
+            f f c 3 c c 3 c c f b c b b c . 
+            f f b 3 b c 3 b c f b c c b c . 
+            . c b b b b b b c b b c c c . . 
+            . c 1 b b b 1 b b c c c c . . . 
+            c b b b b b b b b b c c . . . . 
+            c b c b b b c b b b b f . . . . 
+            f b 1 f f f 1 b b b b f c . . . 
+            f b b b b b b b b b b f c c . . 
+            . f b b b b b b b b c f . . . . 
+            . . f b b b b b b c f . . . . . 
+            . . . f f f f f f f . . . . . . 
+            `, SpriteKind.Enemy)
+        tiles.placeOnRandomTile(enemy1, assets.tile`transparency16`)
+        enemy1.follow(mySprite, 80)
+    }
+    for (let index = 0; index < numOfEnemy; index++) {
+        enemy1 = sprites.create(img`
+            . . . c c c c c c . . . . . . . 
+            . . c 6 7 7 7 7 6 c . . . . . . 
+            . c 7 7 7 7 7 7 7 7 c . . . . . 
+            c 6 7 7 7 7 7 7 7 7 6 c . . . . 
+            c 7 c 6 6 6 6 c 7 7 7 c . . . . 
+            f 7 6 f 6 6 f 6 7 7 7 f . . . . 
+            f 7 7 7 7 7 7 7 7 7 7 f . . . . 
+            . f 7 7 7 7 6 c 7 7 6 f . . . . 
+            . . f c c c c 7 7 6 f c c c . . 
+            . . c 6 2 7 7 7 f c c 7 7 7 c . 
+            . c 6 7 7 2 7 7 c f 6 7 7 7 7 c 
+            . c 1 1 1 1 7 6 6 c 6 6 6 c c c 
+            . c 1 1 1 1 1 6 6 6 6 6 6 c . . 
+            . c 6 1 1 1 1 1 6 6 6 6 6 c . . 
+            . . c 6 1 1 1 1 1 7 6 6 c c . . 
+            . . . c c c c c c c c c c . . . 
+            `, SpriteKind.Enemy)
+        tiles.placeOnRandomTile(enemy1, assets.tile`transparency16`)
+        enemy1.follow(mySprite, 20)
+    }
+}
 sprites.onOverlap(SpriteKind.Player, SpriteKind.kindItem1, function (sprite, otherSprite) {
     if (otherSprite.image.equals(img`
         . . . . . . . . . . . . . . . . 
@@ -144,7 +275,7 @@ function setRenga () {
     for (let cY = 0; cY <= 13; cY++) {
         for (let cX = 0; cX <= 13; cX++) {
             if (tiles.tileAtLocationEquals(tiles.getTileLocation(cY, cX), assets.tile`transparency16`)) {
-                if (Math.percentChance(40)) {
+                if (Math.percentChance(10 + stageLevel * 7)) {
                     renga2 = sprites.create(img`
                         e e e e e e e e e e f e e e e e 
                         e e e e e e e e e e f e e e e e 
@@ -187,9 +318,10 @@ function setRenga () {
         e e e e f e e e e e e e e e e e 
         f f f f f f f f f f f f f f f f 
         `, SpriteKind.goal)
+    goalVisible = false
     tiles.placeOnRandomTile(spriteGoal, assets.tile`transparency16`)
     tiles.setWallAt(tiles.getTileLocation(spriteGoal.x / 16, spriteGoal.y / 16), true)
-    if (Math.percentChance(10)) {
+    if (stageLevel >= 4) {
         ItemDamage = sprites.create(img`
             e e e e e e e e e e f e e e e e 
             e e e e e e e e e e f e e e e e 
@@ -405,10 +537,6 @@ sprites.onOverlap(SpriteKind.fire, SpriteKind.spriteItem1, function (sprite, oth
         `)
     tiles.setWallAt(tiles.getTileLocation(otherSprite.x / 16, otherSprite.y / 16), false)
 })
-sprites.onOverlap(SpriteKind.Eraser, SpriteKind.renga, function (sprite, otherSprite) {
-    otherSprite.destroy()
-    sprite.destroy()
-})
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.x == 24 && mySprite.y == 24) {
         if (mySprite.image.equals(img`
@@ -436,14 +564,27 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
         }
     }
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy2, function (sprite, otherSprite) {
+    game.over(false)
+})
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.renga, function (sprite, otherSprite) {
-    if (onlyStart == 1) {
+    if (onlyStart) {
         tiles.placeOnRandomTile(sprite, assets.tile`transparency16`)
     }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.goal, function (sprite, otherSprite) {
-    if (numOfEnemy <= 0) {
-        game.over(true, effects.dissolve)
+    if (goalVisible) {
+        if (numOfEnemy <= 0) {
+            if (stageLevel < 5) {
+                game.showLongText("Stage " + convertToText(stageLevel) + " crear", DialogLayout.Center)
+                stageLevel += 1
+                clearStage()
+                setRenga()
+                setSprite()
+            } else {
+                game.over(true, effects.dissolve)
+            }
+        }
     }
 })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -473,7 +614,7 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 sprites.onCreated(SpriteKind.Projectile, function (sprite) {
-    timer.after(5000, function () {
+    timer.after(4000, function () {
         tiles.setWallAt(tiles.getTileLocation(sprite.x / 16, sprite.y / 16), false)
         numOfBomb += -1
         music.bigCrash.play()
@@ -604,7 +745,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     game.over(false)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    onlyStart = 0
+    onlyStart = false
     if (numOfBomb < maxbomb) {
         mySprite2 = sprites.create(img`
             . . . . . . . . . . 2 2 2 2 . . 
@@ -641,88 +782,24 @@ let Item1: Sprite = null
 let ItemDamage: Sprite = null
 let spriteGoal: Sprite = null
 let renga2: Sprite = null
+let goalVisible = false
 let enemy1: Sprite = null
 let mySprite: Sprite = null
 let fireDamage = false
-let goalVisible = false
 let numOfEnemy = 0
+let onlyStart = false
 let numOfBomb = 0
 let maxbomb = 0
 let bombpower = 0
-let onlyStart = 0
-onlyStart = 1
+let stageLevel = 0
+stageLevel = 1
 bombpower = 1
 maxbomb = 1
 numOfBomb = 0
+onlyStart = true
 numOfEnemy = 4
-goalVisible = false
 fireDamage = true
 scene.setBackgroundColor(6)
 tiles.setTilemap(tilemap`レベル1`)
 setRenga()
-mySprite = sprites.create(img`
-    . . . . f f f f . . . . . 
-    . . f f f f f f f f . . . 
-    . f f f f f f c f f f . . 
-    f f f f f f c c f f f c . 
-    f f f c f f f f f f f c . 
-    c c c f f f e e f f c c . 
-    f f f f f e e f f c c f . 
-    f f f b f e e f b f f f . 
-    . f 4 1 f 4 4 f 1 4 f . . 
-    . f e 4 4 4 4 4 4 e f . . 
-    . f f f e e e e f f f . . 
-    f e f b 7 7 7 7 b f e f . 
-    e 4 f 7 7 7 7 7 7 f 4 e . 
-    e e f 6 6 6 6 6 6 f e e . 
-    . . . f f f f f f . . . . 
-    . . . f f . . f f . . . . 
-    `, SpriteKind.Player)
-mySprite.setPosition(24, 24)
-controller.moveSprite(mySprite)
-scene.cameraFollowSprite(mySprite)
-for (let index = 0; index < numOfEnemy; index++) {
-    enemy1 = sprites.create(img`
-        . . . c c c c c c . . . . . . . 
-        . . c 6 7 7 7 7 6 c . . . . . . 
-        . c 7 7 7 7 7 7 7 7 c . . . . . 
-        c 6 7 7 7 7 7 7 7 7 6 c . . . . 
-        c 7 c 6 6 6 6 c 7 7 7 c . . . . 
-        f 7 6 f 6 6 f 6 7 7 7 f . . . . 
-        f 7 7 7 7 7 7 7 7 7 7 f . . . . 
-        . f 7 7 7 7 6 c 7 7 6 f . . . . 
-        . . f c c c c 7 7 6 f c c c . . 
-        . . c 6 2 7 7 7 f c c 7 7 7 c . 
-        . c 6 7 7 2 7 7 c f 6 7 7 7 7 c 
-        . c 1 1 1 1 7 6 6 c 6 6 6 c c c 
-        . c 1 1 1 1 1 6 6 6 6 6 6 c . . 
-        . c 6 1 1 1 1 1 6 6 6 6 6 c . . 
-        . . c 6 1 1 1 1 1 7 6 6 c c . . 
-        . . . c c c c c c c c c c . . . 
-        `, SpriteKind.Enemy)
-    tiles.placeOnRandomTile(enemy1, assets.tile`transparency16`)
-    enemy1.follow(mySprite, 20)
-}
-for (let index = 0; index < 1; index++) {
-    numOfEnemy += 1
-    enemy1 = sprites.create(img`
-        . . f f f . . . . . . . . f f f 
-        . f f c c . . . . . . f c b b c 
-        f f c c . . . . . . f c b b c . 
-        f c f c . . . . . . f b c c c . 
-        f f f c c . c c . f c b b c c . 
-        f f c 3 c c 3 c c f b c b b c . 
-        f f b 3 b c 3 b c f b c c b c . 
-        . c b b b b b b c b b c c c . . 
-        . c 1 b b b 1 b b c c c c . . . 
-        c b b b b b b b b b c c . . . . 
-        c b c b b b c b b b b f . . . . 
-        f b 1 f f f 1 b b b b f c . . . 
-        f b b b b b b b b b b f c c . . 
-        . f b b b b b b b b c f . . . . 
-        . . f b b b b b b c f . . . . . 
-        . . . f f f f f f f . . . . . . 
-        `, SpriteKind.Enemy)
-    tiles.placeOnRandomTile(enemy1, assets.tile`transparency16`)
-    enemy1.follow(mySprite, 100)
-}
+setSprite()
