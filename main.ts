f1c83a1e3e3efb28fe5460e6f4413529fe30b6c1
some8a <67367308+some8a@users.noreply.github.com>
2021-07-27 09:34:35 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -816,7 +816,24 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     onlyStart = false
-    if (true) {
+    if (!(spriteGoal.image.equals(img`
+        f f f f f f f f f f f f f f f f 
+        f e e e f f f f f f f f e e e f 
+        f e 4 f f f f f f c f f f 4 4 f 
+        f e f f f f f f c c f f f c 4 f 
+        b e f f f c f f f f f f f c 4 f 
+        b e c c c f f f e e f f c c 4 f 
+        f e f f f f f e e f f c c f 4 f 
+        f e f f f b f e e f b f f f 4 f 
+        f e 4 f 4 1 f 4 4 f 1 4 f 4 4 f 
+        f e 4 f e 4 4 4 4 4 4 e f 4 4 f 
+        f e 4 f f f e e e e f f f 4 4 f 
+        f e f e f b 7 7 7 7 b f e f 4 f 
+        b e e 4 f 7 7 7 7 7 7 f 4 e 4 f 
+        b e e e f 6 6 6 6 6 6 f e e 4 f 
+        f e 4 4 4 f f f f f f 4 4 4 4 f 
+        f e 4 4 4 f f 4 4 f f 4 4 4 4 f 
+        `))) {
         if (numOfBomb < maxbomb) {
             mySprite2 = sprites.create(img`
                 . . . . . . . . . . 2 2 2 2 . . 
